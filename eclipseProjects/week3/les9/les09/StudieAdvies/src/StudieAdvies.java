f021848ce8ebc62c72a9e6b5618320d6ce005680
synolog1x <sanderrdtc@gmail.com>
2018-03-02 14:17:11 +0100

    les09 studieadvies

---
+++ eclipseProjects/week3/les9/les09/StudieAdvies/src/StudieAdvies.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class StudieAdvies {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+
+		Student s1 = new Student("persoon 1");
+		for (int i = 0; i < 8; i++) {
+			s1.setCijfer(i, r.nextInt(10) + 1);
+		}
+
+		System.out.println(s1);
+		System.out.println(Studieadviseur.krijgtPositiefStudieadvies(s1));
+		System.out.println("------------------");
+	}
+
+}
---
+++ eclipseProjects/week3/les9/les09/StudieAdvies/src/Studieadviseur.java
@@ -0,0 +1,19 @@
+public class Studieadviseur {
+
+	public static boolean krijgtPositiefStudieadvies(Student s) {
+		int aantalZesOfHoger = 0;
+		for (int i = 0; i < s.getCijfers().length; i++) {
+			if (s.getCijfers()[i] >= 6) {
+				aantalZesOfHoger++;
+			}
+
+			
+
+		}
+		if (aantalZesOfHoger >= 4) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
